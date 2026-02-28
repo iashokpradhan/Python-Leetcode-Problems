6b1e2c5cb891f88157c9d6a6f6c1c6f02169511f
iashokpradhan <ashok.work.144@gmail.com>
2026-02-28 10:23:15 +0530

    Time: 6 ms (53.55%) | Memory: 22.7 MB (26.75%) - LeetSync

---
+++ 155-min-stack/min-stack.py
@@ -0,0 +1,21 @@
+class MinStack:
+    def __init__(self):
+        self.stack = []  # we store tuples: (value, current_min)
+
+    def push(self, val: int) -> None:
+        if not self.stack:
+            current_min = val
+        else:
+            current_min = min(val, self.getMin())
+        
+        self.stack.append((val, current_min))
+
+    def pop(self) -> None:
+        if self.stack:
+            self.stack.pop()
+
+    def top(self) -> int:
+        return self.stack[-1][0] if self.stack else None
+
+    def getMin(self) -> int:
+        return self.stack[-1][1] if self.stack else None
